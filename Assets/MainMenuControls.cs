2da85b0cd55d7f277175136f6a6f82379d621de4
Ali Haider <alihaider1793@gmail.com>
2018-03-16 14:11:16 +0500

    main menu added
    main menu added

---
+++ Assets/MainMenuControls.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenuControls : MonoBehaviour {
+
+	public void LoadGame()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+
+    public void QuitGame()
+    {
+        Debug.Log("EXITING APPLICATION FROM MAIN MENU");
+        Application.Quit();
+    }
+}
