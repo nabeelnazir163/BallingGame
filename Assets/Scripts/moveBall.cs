08f8f4888f45430a81e90dc01903fefa8157dde0
Ali Haider <alihaider1793@gmail.com>
2018-03-19 15:05:21 +0500

    19/3/18 60% there

---
+++ Assets/Scripts/moveBall.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class moveBall : MonoBehaviour {
+
+    private CharacterController control;
+    private float speed = 5f;
+    private Vector3 vec;
+
+	// Use this for initialization
+	void Start () {
+        control = GetComponent<CharacterController>();
+		
+	}
+
+    // Update is called once per frame
+    void Update()
+    {
+        //vec = Vector3.zero;
+        //vec.x = Input.GetAxisRaw("Horizontal") * speed;
+
+        control.Move(Vector3.right *speed * Time.deltaTime);
+    }
+}
